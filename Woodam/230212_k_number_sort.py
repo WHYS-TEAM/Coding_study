8079fed7c2fccf4466e5133e464bf5fefad9dac2
Woodam Lim <woodam.lim@gmail.com>
2023-02-12 19:51:44 +0900

    Create 230212_k_number_sort.py

---
+++ Woodam/230212_k_number_sort.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    
+    for j in commands :
+        temp = []
+        temp = array[j[0]-1:j[1]] # 0부터 시작하니까 -1
+        temp.sort() # 정렬
+        answer.append(temp[j[2]-1]) # 0부터 시작하니까 -1
+        
+    return answer
