4f813aa8ca30cffb61ead8d81562a38c0f9cbd15
Snag <52437923+Snag9311@users.noreply.github.com>
2023-04-30 18:27:00 +0900

    Create 230430_Lv2_H-index.py

---
+++ Sangyoung/230430_Lv2_H-index.py
@@ -0,0 +1,15 @@
+def solution(citations):
+    
+    # sort  
+    citations.sort()
+    
+    h_list  = []  # possible h-idx values
+    for h in range(len(citations)+1):  # '전체' 논문 수 포함해서 iter. see test case: [1]
+        c = len([x for x in citations if x >= h])  # h이상 인용 논문 count
+        if c >= h:  # "h번 이상 인용된 논문(c)이 h편 이상"이면
+            h_list.append(h)
+    
+    print(h_list)
+    
+    return max(h_list)
+    
